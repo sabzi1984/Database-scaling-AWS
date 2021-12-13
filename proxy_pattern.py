a24ac39561677a7059443eebf80a2284fde0c104
ALI Akbar SABZI <57127882+sabzi1984@users.noreply.github.com>
2021-12-13 16:17:35 -0500

    Add files via upload

--- proxy_pattern.py
+++ proxy_pattern.py
@@ -25,97 +25,97 @@ def main():
     listen = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     listen.bind(('', 5001))   #socket.gethostname()
     listen.listen(1)
-    while True:
-        conn, addr = listen.accept()
-        print(f"Connection from {addr} has been established.")
-        with conn:
-            print('Connected by', addr)
+    conn, addr = listen.accept()
+    print(f"Connection from {addr} has been established.")
+    with conn:
+        print('Connected by', addr)
 
-            while True:
-                data = conn.recv(2048) 
-                if not data:
-                    break
+        while True:
+            data = conn.recv(2048) 
+            if not data:
+                break
+            
+            cmd_type, command = load_data(data)
+            
+            if cmd_type=="insert" and mode=='direct':
+                targ=0
+                target_node="master"
+                cnx = mysql.connector.connect(user='proxy', password='alfi1326', host=targets[targ]["ip"], database='tp3')
+                print ('Connection to DB opened')
+                cursor = cnx.cursor()
+                cursor.execute(command)
+                cnx.commit()
+                response = {'handled by':target_node, 'response': "OK"}
+                response = pickle.dumps(response)
+                conn.send(response)
                 
-                cmd_type, command = load_data(data)
                 
-                if cmd_type=="insert" and mode=='direct':
-                    targ=0
-                    target_node="master"
-                    cnx = mysql.connector.connect(user='proxy', password='alfi1326', host=targets[targ]["ip"], database='tp3')
-                    print ('Connection to DB opened')
-                    cursor = cnx.cursor()
-                    cursor.execute(command)
-                    cnx.commit()
-                    response = {'handled by':target_node, 'response': "OK"}
-                    response = pickle.dumps(response)
-                    conn.send(response)
-                    
-                    
-                if cmd_type=="select" and mode=='direct':
-                    targ=0
-                    target_node="master"
-                    cnx = mysql.connector.connect(user='proxy', password='alfi1326', host=targets[targ]["ip"], database='tp3')
-                    print ('Connection to DB opened')
-                    cursor = cnx.cursor()
-                    cursor.execute(command)
-                    print(f'handled by :{target_node}')
-                    result = cursor.fetchall()
-                    response={'handled by':{target_node}, 'result':result}
-                    response = pickle.dumps(response)
-                    
-                if cmd_type=="select" and mode=='random':
-                    targ=randint(1, 3)
-                    target_node="slave"+str(targ)
+            if cmd_type=="select" and mode=='direct':
+                targ=0
+                target_node="master"
+                cnx = mysql.connector.connect(user='proxy', password='alfi1326', host=targets[targ]["ip"], database='tp3')
+                print ('Connection to DB opened')
+                cursor = cnx.cursor()
+                cursor.execute(command)
+                print(f'handled by :{target_node}')
+                result = cursor.fetchall()
+                response={'handled by':{target_node}, 'result':result}
+                response = pickle.dumps(response)
+                conn.send(response)
+                
+            if cmd_type=="select" and mode=='random':
+                targ=randint(1, 3)
+                target_node="slave"+str(targ)
 
-                    cnx = mysql.connector.connect(user='proxy', password='alfi1326', host=targets[targ]["ip"], database='tp3')
-                    print ('Connection to DB opened')
-                    cursor = cnx.cursor()
-                    cursor.execute(command)
-                    print('handled by :{target_node}')
-                    result = cursor.fetchall()
-                    response={'handled by' :{target_node}, 'result':result}
-                    response = pickle.dumps(response)
-                    
-                    conn.send(result)
-                    # for (Series_reference, Period, Data_value, Status, Units, Magnitude, Series_title_1) in cursor:
-                    #     print (f"{Series_reference}, {Period, Data_value}, {Status, Units}, {Magnitude}, {Series_title_1}")
-                    
-                if cmd_type=="select" and mode=='custom':
-                    targ=custom()
-                    target_node="slave"+str(targ)
-                    cnx = mysql.connector.connect(user='proxy', password='alfi1326', host=targets[targ]["ip"], database='tp3')
-                    print ('Connection to DB opened')
-                    cursor = cnx.cursor()
-                    cursor.execute(command)
-                    print('handled by :{target_node}')
-                    result = cursor.fetchall()
-                    response={'handled by' :{target_node}, 'result':result}
-                    response = pickle.dumps(response)
-                    # for (Series_reference, Period, Data_value, Status, Units, Magnitude, Series_title_1) in cursor:
-                    #     print (f"{Series_reference}, {Period, Data_value}, {Status, Units}, {Magnitude}, {Series_title_1}")
+                cnx = mysql.connector.connect(user='proxy', password='alfi1326', host=targets[targ]["ip"], database='tp3')
+                print ('Connection to DB opened')
+                cursor = cnx.cursor()
+                cursor.execute(command)
+                print('handled by :{target_node}')
+                result = cursor.fetchall()
+                response={'handled by' :{target_node}, 'result':result}
+                response = pickle.dumps(response)
                 
+                conn.send(result)
+                # for (Series_reference, Period, Data_value, Status, Units, Magnitude, Series_title_1) in cursor:
+                #     print (f"{Series_reference}, {Period, Data_value}, {Status, Units}, {Magnitude}, {Series_title_1}")
                 
-                # send = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                # send.connect((targets[targ]["ip"], targets[targ]["port"]))
-                # cnx = mysql.connector.connect(user='root', password='alfi1326', host=targets[targ]["ip"], database='tp3')
-                # print ('Connection to DB opened')
-                # cursor = cnx.cursor()
-                # if cmd_type == 'insert':
-                # cursor.execute(command)
-                # cnx.commit()
-                
-                # send = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                
+            if cmd_type=="select" and mode=='custom':
+                targ=custom()
+                target_node="slave"+str(targ)
+                cnx = mysql.connector.connect(user='proxy', password='alfi1326', host=targets[targ]["ip"], database='tp3')
+                print ('Connection to DB opened')
+                cursor = cnx.cursor()
+                cursor.execute(command)
+                print('handled by :{target_node}')
+                result = cursor.fetchall()
+                response={'handled by' :{target_node}, 'result':result}
+                response = pickle.dumps(response)
+                # for (Series_reference, Period, Data_value, Status, Units, Magnitude, Series_title_1) in cursor:
+                #     print (f"{Series_reference}, {Period, Data_value}, {Status, Units}, {Magnitude}, {Series_title_1}")
+            
+            
+            # send = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            # send.connect((targets[targ]["ip"], targets[targ]["port"]))
+            # cnx = mysql.connector.connect(user='root', password='alfi1326', host=targets[targ]["ip"], database='tp3')
+            # print ('Connection to DB opened')
+            # cursor = cnx.cursor()
+            # if cmd_type == 'insert':
+            # cursor.execute(command)
+            # cnx.commit()
+            
+            # send = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            
 
-                # response = pickle.dumps(response)
-                # # send.send(pickledobj)
-                
-                # # response =' handled by node ' + str(targ)
-                # conn.send(response)
+            # response = pickle.dumps(response)
+            # # send.send(pickledobj)
+            
+            # # response =' handled by node ' + str(targ)
+            # conn.send(response)
 
-        print ('Will close socket')
-        # send.close()
-        listen.close()
+print ('Will close socket')
+# send.close()
+listen.close()
 
 
 def load_data(data):
